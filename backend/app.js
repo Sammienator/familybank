fb4729b9fb6dc700dcc400a6e2a6618bb5859387
Sammienator <sammugo18@gmail.com>
2022-07-17 23:56:49 +0300

    changes

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use("/events", router); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 
 mongoose
   .connect(
